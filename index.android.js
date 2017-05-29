226073e4b9604ac62b19b3a8bdc329ab9e8bfc82
Jaghit Singh <iamjaghitsingh@gmail.com>
2017-05-29 12:05:07 +0530

    Working fine

---
+++ index.android.js
@@ -0,0 +1,51 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+import MapView from 'react-native-maps';
+
+export default class reactnativeMAP extends Component {
+    render() {
+        const { region } = this.props;
+        console.log(region);
+
+        return (
+            <View style={styles.mapcontainer}>
+                <MapView
+                    style={styles.map}
+                    region={{
+                        latitude: 37.78825,
+                        longitude: -122.4324,
+                        latitudeDelta: 0.015,
+                        longitudeDelta: 0.0121,
+                    }}
+                >
+                </MapView>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    mapcontainer: {
+        ...StyleSheet.absoluteFillObject,
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject,
+    },
+});
+
+AppRegistry.registerComponent('reactnativeMAP', () => reactnativeMAP);
